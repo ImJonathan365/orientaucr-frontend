313e0cad2459f8a5f33ac3b34ac00292f5bbee5f
ImJonathan365 <jonathan365.git@gmail.com>
2025-05-19 14:57:08 -0600

    Redireción a página de perfil de usuario

---
+++ src/pages/user/UserProfile.tsx
@@ -0,0 +1,5 @@
+export const UserProfile = () => {
+    return (
+        <div><center><h1>Perfil de usuario</h1></center></div>
+    )
+};
--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -5,6 +5,7 @@ import { AcademicCentersPage } from "../pages/home/AcademicCentersPage";
 import { VocationalTestPage } from "../pages/home/VocationalTestPage";
 import { SimulationTestPage } from "../pages/home/SimulationTestPage";
 import { CareerListPage } from "../pages/CareerListPage";
+import { UserProfile } from '../pages/user/UserProfile'
 
 export default function PrivateRoutes() {
   return (
@@ -16,6 +17,7 @@ export default function PrivateRoutes() {
         <Route path="/vocational-test" element={<VocationalTestPage />} />
         <Route path="/simulation-test" element={<SimulationTestPage />} />
         <Route path="/career-list" element={<CareerListPage />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Route>
     </Routes>
   );
